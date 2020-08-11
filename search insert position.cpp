a871f6614b8770cb6ee29dc640398c9ce961df35
Pankaj Kumar Sahu <pankajsahuhero@gmail.com>
2020-08-11 20:15:14 +0530

    Update search insert position.cpp

--- search insert position.cpp
+++ search insert position.cpp
@@ -22,3 +22,19 @@ public:
         return lower_bound(nums.begin(),nums.end(),target) - nums.begin();
     }  
 };
+
+// tranversing from back side
+// Runtime: 12 ms, faster than 57.35% of C++ online submissions for Search Insert Position.
+//Memory Usage: 9.9 MB, less than 22.85% of C++ online submissions for Search Insert Position.
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        
+        if(nums[nums.size()-1] < target ) return nums.size();
+        for(int i= nums.size()-1; i>=0; i--){
+            if(nums[i]==target) return i;
+            if(nums[i]<target) return i+1;
+        }
+         return 0;
+    }  
+};
