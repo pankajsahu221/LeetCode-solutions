0f2c2b48abe57d54162b7724edff004113d92a19
Pankaj Kumar Sahu <pankajsahuhero@gmail.com>
2020-08-18 22:35:26 +0530

    Update Longest Substring Without Repeating Characters.cpp

--- Longest Substring Without Repeating Characters.cpp
+++ Longest Substring Without Repeating Characters.cpp
@@ -1,4 +1,4 @@
-//brute force
+//brute force O(n^2)
 //Runtime: 60 ms, faster than 35.27% of C++ online submissions for Longest Substring Without Repeating Characters.
 //Memory Usage: 7 MB, less than 86.44% of C++ online submissions for Longest Substring Without Repeating Characters.
 
@@ -21,5 +21,29 @@ public:
          maxi = max(locu, maxi);  
       }
         return maxi;        
+    }
+};
+
+// two pointer O(n)
+// Runtime: 56 ms, faster than 39.43% of C++ online submissions for Longest Substring Without Repeating Characters.
+// Memory Usage: 10.9 MB, less than 28.97% of C++ online submissions for Longest Substring Without Repeating Characters.
+class Solution {
+    public:
+    int lengthOfLongestSubstring(string s) {
+      
+        int i=0, j=0, maxi=0, n=s.size();
+        unordered_set<char> set;
+    
+        while(i<n && j<n){
+            if(set.find(s[j]) == set.end()){    //if the char is not present in the set
+
+                set.insert(s[j++]);    //add it to the set
+                maxi = max(maxi , j-i );  //increase it if it's greater
+             }
+            else{                 //if char is present in the set
+                set.erase(s[i++]);//we erase the first occurence of the char in the set
+             }
+           }
+    return maxi;
     }
 };
